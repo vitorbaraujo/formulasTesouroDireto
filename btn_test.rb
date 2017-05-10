7b3c96ef07930ab918c530fe98bcaa54b436bc2f
Vitor Barbosa <vitornga15@gmail.com>
2017-05-10 16:58:09 -0300

    Add another test to calculate unit price

--- btn_test.rb
+++ btn_test.rb
@@ -2,10 +2,17 @@ require 'minitest/autorun'
 require_relative 'btn'
 
 class BtnTest < Minitest::Test
-  def test_should_calculate_unity_price
+  def test_should_calculate_unit_price
     pu_ant = 1000.000000
     tr = 0.230000
     result = Btn.calculate_unit_price(pu_ant, tr)
     assert_in_delta 230.000000, result, 0.0000001
+  end
+
+  def test_should_calculate_another_unit_price
+    pu_ant = 500.000000 
+    tr = 0.230000
+    result = Btn.calculate_unit_price(pu_ant, tr)
+    assert_in_delta 115.000000, result, 0.0000001
   end
 end
