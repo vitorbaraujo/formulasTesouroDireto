525ba3699e9730b0f0af6bfae34100845823e8c6
Vitor Barbosa <vitornga15@gmail.com>
2017-05-10 17:24:35 -0300

    Add test to check truncation

--- btn_test.rb
+++ btn_test.rb
@@ -1,33 +1,45 @@
 require 'minitest/autorun'
 require_relative 'btn'
 
+EPS = 0.0000001
+
 class BtnTest < Minitest::Test
   def test_should_calculate_unit_price
     pu_ant = 1000.000000
     tr = 0.230000
     result = Btn.calculate_unit_price(pu_ant, tr)
-    assert_in_delta 230.000000, result, 0.0000001
+    assert_in_delta 230.000000, result, EPS
   end
 
   def test_should_calculate_another_unit_price
     pu_ant = 500.000000
     tr = 0.230000
     result = Btn.calculate_unit_price(pu_ant, tr)
-    assert_in_delta 115.000000, result, 0.0000001
+    assert_in_delta 115.000000, result, EPS
   end
 
   def test_should_calculate_right_even_with_less_decimals
     pu_ant = 500.00
     tr = 0.230
     result = Btn.calculate_unit_price(pu_ant, tr)
-    assert_in_delta 115.000000, result, 0.0000001
+    assert_in_delta 115.000000, result, EPS
   end
 
   def test_should_calculate_right_even_with_more_decimals
     pu_ant = 500.0000000000000000000
     tr = 0.23000000000000000000
     result = Btn.calculate_unit_price(pu_ant, tr)
-    assert_in_delta 115.000000, result, 0.0000001
+    assert_in_delta 115.000000, result, EPS
+  end
+
+  def test_should_truncate_previous_price_before_calculate_unit_price
+    pu_ant = 0.0000019
+    tr = 1000000
+
+    result = Btn.calculate_unit_price(pu_ant, tr)
+
+    assert_equal (result - 1.9) <= EPS, false
+    assert_in_delta 1.0, result, EPS, "quebrou 1"
   end
 
 end
