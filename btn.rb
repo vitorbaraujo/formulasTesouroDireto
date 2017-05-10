4cb4dcb1c3ebc634931b957b05c61ebc01a66d40
Victor Navarro <victor.matias.navarro@gmail.com>
2017-05-10 16:49:30 -0300

    Initialize project and create first test

---
+++ btn.rb
@@ -0,0 +1,2 @@
+class Btn
+end
---
+++ btn_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require_relative 'btn'
+
+class BtnTest < Minitest::Test
+  def test_should_calculate_unity_price
+    pu_ant = 1000.000000
+    tr = 0.230000
+    result = Btn.calculate_unity_price(pu_ant, tr)
+    assert_in_delta 230.000000, result, 0.0000001
+  end
+end
