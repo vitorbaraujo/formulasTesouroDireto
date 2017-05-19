4fee566094b7bc0bdd09d7a366daf8e3a9e4a53a
Vitor Barbosa <vitornga15@gmail.com>
2017-05-19 16:00:28 -0300

    Add method to calculate ntn_a interest and sum of factors

--- ntn_a.rb
+++ ntn_a.rb
@@ -3,12 +3,26 @@ require 'bigdecimal/util'
 
 class NtnA
 
-  def self.calculate_amortization_unit_price uss_a, uss_b, liquid_value
+  def self.calculate_amortization_unit_price uss_a, uss_b, emission_value, amort_perc
+    liquid_value = calculate_liquid_value(emission_value, amort_perc)
     (((uss_a * 1.0)/ uss_b) * liquid_value).round(6)
   end
 
   def self.calculate_liquid_value emission_value, amort_perc
     emission_value * amort_perc
+  end
+
+  def self.calculate_interest amortized_unit_price, factor_list
+    (amortized_unit_price * factor_list.reduce(:+)).to_d.truncate(6).to_f
+  end
+
+  def self.calculate_interests_sum factor_list
+    sum = 0
+    factor_list.each do |factor|
+      sum = sum + (((factor[:dtp] - factor[:dtup]) / 360.0) * (factor[:i] / 100.0))
+    end
+
+    sum
   end
 
 end
