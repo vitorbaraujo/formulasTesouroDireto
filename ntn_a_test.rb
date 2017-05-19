40bf2d339cea991480e183b537040ded814f1d8d
Vitor Barbosa <vitornga15@gmail.com>
2017-05-19 15:59:57 -0300

    Add tests to calculate ntn_a interest

--- ntn_a_test.rb
+++ ntn_a_test.rb
@@ -6,9 +6,10 @@ class NtnATest < Minitest::Test
   def test_should_calculate_amortization_unit_price
     uss_a = 3.546
     uss_b = 2.998
-    liquid_value = 34
+    emission_value = 17
+    amort_perc = 2
 
-    result = NtnA.calculate_amortization_unit_price(uss_a, uss_b, liquid_value)
+    result = NtnA.calculate_amortization_unit_price(uss_a, uss_b, emission_value, amort_perc)
 
     assert_equal 40.214810, result
   end
@@ -16,9 +17,10 @@ class NtnATest < Minitest::Test
   def test_should_calculate_another_amortization_unit_price
     uss_a = 2.3
     uss_b = 2.4
-    liquid_value = 34
+    emission_value = 17
+    amort_perc = 2
 
-    result = NtnA.calculate_amortization_unit_price(uss_a, uss_b, liquid_value)
+    result = NtnA.calculate_amortization_unit_price(uss_a, uss_b, emission_value, amort_perc)
 
     assert_equal 32.583333, result
   end
@@ -39,6 +41,32 @@ class NtnATest < Minitest::Test
     result = NtnA.calculate_liquid_value(emission_value, amort_perc)
 
     assert_equal 26.372784, result
+  end
+
+  def test_should_calculate_interest_truncating_with_six_decimal_places
+    amort_unit_price = 20
+    factor_list = [1.2345678]
+
+    result = NtnA.calculate_interest(amort_unit_price, factor_list)
+
+    assert_equal 24.691356, result
+  end
+
+  def test_should_calculate_another_interest
+    amort_unit_price = 30
+    factor_list = [1, 2, 3, 4.5]
+
+    result = NtnA.calculate_interest(amort_unit_price, factor_list)
+
+    assert_equal 315.000000, result
+  end
+
+  def test_should_calculate_sum_of_interest_rates
+    factor_list = [ {dtp: 23, dtup: 20, i: 0.41}, {dtp: 29, dtup: 16, i: 0.32}, {dtp: 14, dtup: 7, i: 0.78}]
+
+    result = NtnA.calculate_interests_sum(factor_list)
+
+    assert_equal 0.0003013888888888889, result
   end
 
 end
