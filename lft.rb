ebe926de40bb528d09ed2801b344170410e4d559
Victor Navarro <victor.matias.navarro@gmail.com>
2017-05-22 18:55:44 -0300

    Add method to calculate nominal_value_a in LFT

--- lft.rb
+++ lft.rb
@@ -16,4 +16,8 @@ class Lft
     (((((selic.to_d.truncate(2).to_f/100)+1)**(1/252.0))-1).round(9)).to_d.truncate(8).to_f
   end
 
+  def self.calculate_nominal_value_a c, ve, tac, du, dut, pa, n
+    ((c*(ve).to_d.truncate(6).to_f*(1+((tac/100.0)**(du/(dut*1.0)))))-(c*(ve).to_d.truncate(6).to_f*((1+tac)**(du/(dut*1.0))))*(1.0/((pa-n)*1.0))).to_d.truncate(6).to_f
+  end
+
 end
